79da7b53ac8c225e327e4a39bdb8f3f27e57bdaf
Teun Wolbert <teunwolbert@gmail.com>
2023-09-21 21:39:34 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,213 @@
+﻿using System.Net;
+using System.Text;
+
+namespace C__webserver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLiteUtils.init();
+            Console.WriteLine("Database created");
+
+            Task.Run(() => Start(args)).GetAwaiter().GetResult();
+        }
+
+        static async Task Start(string[] args)
+        {
+            string[] prefixes = { "http://localhost:4201/" };  // Replace with your desired server address
+
+            HttpListener listener = new HttpListener();
+            foreach (string prefix in prefixes)
+                listener.Prefixes.Add(prefix);
+
+            listener.Start();
+            Console.WriteLine("Server started. Listening for requests...");
+            if (!File.Exists("./server.log"))
+                File.Create("./server.log").Close();
+
+            while (true)
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+
+                // Handle requests asynchronously
+                Task.Run(() => HandleRequestAsync(context));
+            }
+        }
+
+        static async Task HandleRequestAsync(HttpListenerContext context)
+        {
+            Console.WriteLine("Handling request..");
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            // Build log
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Request headers:\n");
+            foreach (string key in request.Headers.AllKeys)
+            {
+                stringBuilder.Append($"{key}: {request.Headers[key]}\n");
+            }
+
+            string responseString = "";
+
+            if (request?.Url?.AbsolutePath == "/login")
+            {
+                Console.WriteLine("Hit /login");
+                if (request.HttpMethod != "POST")
+                {
+                    responseString = $"Invalid request method. \"{request.HttpMethod}\"" +
+                        $"\nFull request details:" +
+                        $"\n {request.Headers}" +
+                        $"\n {request.AcceptTypes}" +
+                        $"\n Input stream:" +
+                        $"\n {request.InputStream}";
+                }
+                else
+                {
+                    string res = await handleLogin(request);
+                    // Check request method and respond accordingly
+                    responseString = res;
+                }
+
+
+            }
+            else if (request?.Url?.AbsolutePath == "/signup")
+            {
+                Console.WriteLine("Hit /singup");
+                if (request.HttpMethod != "POST")
+                {
+                    responseString = $"Invalid request method. \"{request.HttpMethod}\"" +
+                        $"\nFull request details:" +
+                        $"\n {request.Headers}" +
+                        $"\n {request.AcceptTypes}" +
+                        $"\n Input stream:" +
+                        $"\n {request.InputStream}";
+                }
+                else
+                {
+                    string res = await handleRegister(request);
+                    // Check request method and respond accordingly
+                    responseString = res;
+                }
+            }
+            else
+            {
+                responseString = "Invalid endpoint.";
+            }
+            stringBuilder.Append("\nRequest body:\n");
+            using (StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding))
+            {
+                stringBuilder.Append(await reader.ReadToEndAsync());
+            }
+            stringBuilder.Append("\n");
+            stringBuilder.Append("Response:\n");
+            stringBuilder.Append(responseString);
+            _ = Task.Run(() => addToLog(stringBuilder.ToString()));
+            
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+
+            // Write the response asynchronously
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.Close();
+        }
+        private static void addToLog(string text)
+        {
+            File.AppendAllText("./server.log", text);
+        }
+        private static async Task<string> handleLogin(HttpListenerRequest request)
+        {
+            // Get post Data from body
+            string postData;
+            using (var reader = new System.IO.StreamReader(request.InputStream, request.ContentEncoding))
+            {
+                postData = await reader.ReadToEndAsync();
+            }
+
+            try
+            {
+                // Parse post data as form data
+                string username = "";
+                string password = "";
+
+                string[] postArray = postData.Split('\n');
+                for (int i = 0; i < postArray.Length - 1; i++)
+                {
+                    if (postArray[i].Contains("username"))
+                    {
+                        username = postArray[i + 2].Trim();
+                    }
+                    if (postArray[i].Contains("password"))
+                    {
+                        password = postArray[i + 2].Trim();
+                    }
+                }
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    return "Invalid username, password or request formatting";
+                }
+                else
+                {
+                    return SQLiteUtils.verifyLogin(username, password);
+                }
+            }
+            catch
+            {
+                return "huh??";
+            }
+        }
+        private static async Task<string> handleRegister(HttpListenerRequest request)
+        {
+            string postData;
+            using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+            {
+                postData = await reader.ReadToEndAsync();
+            }
+
+            try
+            {
+                // Parse post data as form data
+                string username = "";
+                string password = "";
+
+                string[] postArray = postData.Split('\n');
+                for (int i = 0; i < postArray.Length - 1; i++)
+                {
+                    if (postArray[i].Contains("username"))
+                    {
+                        username = postArray[i + 2].Trim();
+                    }
+                    if (postArray[i].Contains("password"))
+                    {
+                        password = postArray[i + 2].Trim();
+                    }
+                }
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    return "Invalid username, password or request formatting";
+                }
+                else
+                {
+                    Task<string> t = SQLiteUtils.registerUser(username, password);
+
+                    t.Wait();
+                    string res = t.Result;
+
+                    if (res != "User added!")
+                    {
+                        return res;
+                    }
+                    else
+                    {
+                        return "User was created!";
+                    }
+                }
+            }
+            catch
+            {
+                return "huh??";
+            }
+        }
+    }
+}
---
+++ SQLiteUtils.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using BCrypt.Net;
+
+namespace C__webserver
+{
+    public static class SQLiteUtils
+    {
+
+        public static SQLiteConnection loginConn;
+        public static void init()
+        {
+            // Create table and database if not exists
+            loginConn = CreateConnectionLogin();
+
+            var cmd = new SQLiteCommand(loginConn)
+            {
+                CommandText = $"CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, password TEXT)"
+            };
+
+            cmd.ExecuteNonQuery();
+        }
+        private static SQLiteConnection CreateConnectionLogin()
+        {
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source= login.db; Version = 3; New = True; Compress = True; ");
+            // Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Couldnt create database");
+            }
+            return sqlite_conn;
+        }
+        public async static Task<string> registerUser(string username, string password)
+        {
+            string checkQuery = "SELECT COUNT(*) FROM users WHERE username = @username";
+            string insertQuery = "INSERT INTO users (username, password) VALUES (@username, @password)";
+
+            try
+            {
+                SQLiteCommand checkCmd = new SQLiteCommand(loginConn)
+                {
+                    CommandText = checkQuery
+                };
+                checkCmd.Parameters.AddWithValue("@username", username);
+                int userCount = Convert.ToInt32(await checkCmd.ExecuteScalarAsync());
+
+                if (userCount == 0)  // Username doesn't exist, so proceed with insertion
+                {
+                    string pw = encryptPassword(password);
+
+                    SQLiteCommand insertCmd = new SQLiteCommand(loginConn)
+                    {
+                        CommandText = insertQuery
+                    };
+                    insertCmd.Parameters.AddWithValue("@username", username);
+                    insertCmd.Parameters.AddWithValue("@password", pw);
+
+                    int result = await insertCmd.ExecuteNonQueryAsync();
+
+                    if (result == 0)
+                    {
+                        return "Something went wrong";
+                    }
+                    else
+                    {
+                        return "User added!";
+                    }
+                }
+                else
+                {
+                    return "Username already exists";
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Console.WriteLine("Error: " + ex.Message);
+                return "Something went wrong";
+            }
+        }
+        
+        public static string verifyLogin(string username, string password)
+        {
+            var cmd = new SQLiteCommand(loginConn);
+            cmd.CommandText = "SELECT * FROM users WHERE username = ?";
+            cmd.Parameters.AddWithValue("@username", username);
+
+            var reader = cmd.ExecuteReader();
+            string passHash = "";
+
+            while (reader.Read())
+            {
+                passHash = reader["password"].ToString();
+            }
+
+            if (string.IsNullOrEmpty(passHash))
+            {
+                return "User doesn't exist!";
+            }
+
+            if (verifyPassword(password, passHash))
+            {
+                return "Login successful!";
+            }
+            else
+            {
+                return "Password incorrect";
+            }
+        }
+        
+        private static bool verifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+        private static string encryptPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, 10);
+        }
+    }
+
+}
